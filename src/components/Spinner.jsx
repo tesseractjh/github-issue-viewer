33608dd69fb663f9f423899034ef1a8a69799428
Tesseract <tensai3355@gmail.com>
2022-10-30 16:15:11 +0900

    :sparkles: 로딩중일 때 표시될 fallback 추가

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,43 @@
+import styled, { keyframes } from 'styled-components';
+
+const SpinnerWrapper = styled.div`
+  ${({ theme }) => theme.placeholder.absoluteCenter};
+  width: 100px;
+  height: 100px;
+`;
+
+const spinnerAnimation = keyframes`
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+`;
+
+const SpinnerQuarter = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 50%;
+  height: 50%;
+  border: 10px solid;
+  border-color: ${({ theme }) => theme.color.ACCENT} transparent transparent transparent;
+  border-radius: 50%;
+  animation: ${spinnerAnimation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  animation-delay: ${({ delay }) => delay ?? 0}s;
+`;
+
+function Spinner() {
+  return (
+    <SpinnerWrapper>
+      <SpinnerQuarter delay={-0.4} />
+      <SpinnerQuarter delay={-0.3} />
+      <SpinnerQuarter delay={-0.2} />
+      <SpinnerQuarter delay={-0.1} />
+      <SpinnerQuarter />
+    </SpinnerWrapper>
+  );
+}
+
+export default Spinner;
