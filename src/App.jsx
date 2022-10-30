56d7e1daf72231d2c330d86f32a38205bdad8a96
Tesseract <tensai3355@gmail.com>
2022-10-30 16:17:06 +0900

    :sparkles: Provider 적용 및 Router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { IssueListProvider } from './contexts/IssueList';
+import Layout from './components/Layout';
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
-  return <div>app</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <IssueListProvider>
+          <Routes>
+            <Route index element={<div>홈</div>} />
+            <Route path="/:organization" element={<Layout />}>
+              <Route index element={<div>단체</div>} />
+              <Route path="/:organization/:repo" element={<div>이슈 목록</div>} />
+              <Route path="/:organization/:repo/:issue" element={<div>이슈</div>} />
+            </Route>
+          </Routes>
+        </IssueListProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './api/axiosDefault';
+import './styles/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
